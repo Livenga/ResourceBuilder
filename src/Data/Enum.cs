12ea885449114fea3ad6a6cc9e05ff1f4f0533ff
Livenga <ljmmz0405@gmail.com>
2017-08-20 16:19:13 +0900

    added summaries

--- src/Data/Enum.cs
+++ src/Data/Enum.cs
@@ -3,6 +3,7 @@ using System.Data;
 
 namespace Live.Data
 {
+  /// <summary>リソースの種類</summary>
   public enum ResourceType : uint {
     Strings = 0,
     Images  = 1,
@@ -11,11 +12,13 @@ namespace Live.Data
     Files   = 4,
   }
 
+  /// <summary>リソースのファイル編集モード</summary>
   public enum ResourceEditMode : uint {
     New  = 0,
     Edit = 1,
   }
 
+  /// <summary>リソースの編集状態</summary>
   public enum ResourceStatus : uint {
     Saved   = 0,
     Updated = 1,
